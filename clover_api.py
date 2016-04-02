5b0ef351f11e04c46d890945bc5aa91f626391f3
Shijie Feng <neonskylife@gmail.com>
2016-04-02 12:20:01 -0700

    initial commit

---
+++ clover_api.py
@@ -0,0 +1,181 @@
+"""Python API wrapper."""
+
+import requests
+import urllib
+import json
+import re
+import config
+
+class CloverResponseObject(dict):
+    """Object to encapsulate response JSON."""
+    def __init__(self, obj):
+        for key, value in obj.items():
+            self.__dict__[key] = value
+        dict.__init__(self, obj)
+
+class CloverAPI(object):
+    """CloverAPI object for making API calls to Clover's server."""
+
+    base_url = "https://www.clover.com"
+    #base_url = "http://localhost:9000" # local
+    # base_url = "https://stg1.dev.clover.com" #staging
+    #base_url = "https://www.clover.com" # production
+    # TODO: change back to production before release
+
+    path_match = re.compile("\{([a-zA-Z]*)\}")
+
+    @classmethod
+    def build_auth_url(cls, callback, app_id, permissions=None):
+        """Builds an url to the Clover authentication server. Send the user to
+        this url to authenticate, and Clover will send an auth-code to the
+        callback.
+        Available permissions:
+            "CUSTOMERS_R"
+            "CUSTOMERS_W"
+            "PAYMENTS_R"
+            "PAYMENTS_W"
+            "INVENTORY_R"
+            "INVENTORY_W"
+            "ORDERS_R"
+            "ORDERS_W"
+            "EMPLOYEES_R"
+            "EMPLOYEES_W"
+            "MERCHANT_R"
+            "MERCHANT_W"
+        Args:
+            callback: Callback URL
+            app_id: Clover App ID from the developer page.
+            permissions: A python list of extra permissions
+        Returns:
+            A string which is the url to send the user to for authentication.
+        """
+        params = {"redirect_uri": callback,
+                  "client_id": app_id}
+
+        if permissions is not None:
+            params["scope"] = ",".join(permissions)
+
+        return "{}/oauth/authorize?{}".format(cls.base_url,
+                                              urllib.urlencode(params))
+
+    def __init__(self, access_token=None, merchant_id=None):
+        """Makes a CloverAPI object.
+        
+        Args:
+            access_token: Token to access Clover's API, appended as a URL
+                          parameter, if present
+            merchant_id: Replaces "{mId}" in the endpoint URL, if present
+        
+        Returns:
+            A CloverAPI object
+        """
+        
+        self.merchant_id = merchant_id
+        self.access_token = access_token
+
+    def get(self, endpoint, **kwargs):
+        """Send a get request using Clover's API
+        
+        Extra query parameters will be formatted in the URL. For example, if
+        itemId is passed in as a keyword argument and "{itemId}" is in the
+        endpoint url, "{itemId}" will be replaced with the value of that
+        argument. If "{itemId}" is not present, the argument will be added as a
+        query parameter in the URL.
+        
+        Args:
+            endpoint: Clover endpoint's url
+            **kwargs: Any other parameters.
+        
+        Returns:
+            A CloverResponseObject converted from the response JSON, which
+            allows access to values in the JSON object. For example, if the JSON
+            object is: {name: "My Merchant Name"}, to get the name from the
+            corresponding CloverResponseObject, use: `response.name`
+        
+        Raises:
+            requests.exceptions.HTTPError: If Clover's server returns an error
+                                           code between 400 and 599.
+        """
+        return self._send("GET", endpoint, **kwargs)
+
+    def post(self, endpoint, data, **kwargs):
+        """Send a post request using Clover's API.
+        See documentation for `get`
+        
+        Args:
+            endpoint: See documentation for `get`
+            data: A python object to convert to a JSON object to send
+            **kwargs: See documentation for `get`
+        
+        Returns:
+            See documentation for `get`
+        
+        Raises:
+            See documentation for `get`
+        """
+        
+        return self._send("POST", endpoint, data, **kwargs)
+
+    def _send(self, method, endpoint, data={}, **kwargs):
+        # Set parameters
+        parameters = kwargs
+        
+        if self.access_token:
+            parameters["access_token"] = self.access_token
+        
+        if self.merchant_id:
+            parameters["mId"] = self.merchant_id
+
+        # Replace user-defined path parameters
+        path_params = CloverAPI.path_match.findall(endpoint)
+        
+        for key in path_params:
+            if key in parameters:
+                endpoint = endpoint.replace('{%s}' % key, parameters[key])
+                parameters.pop(key)
+            else:
+                raise KeyError("Missing path parameter: " + key)
+
+        url = CloverAPI.base_url + endpoint
+
+        # Send request
+        if method == "POST":
+            headers = {}
+            headers["content-type"] = "application/json"
+            response = requests.post(url, headers=headers, params=parameters,
+                                 data=json.dumps(data))
+            
+        elif method == "GET":
+            response = requests.get(url, params=parameters)
+
+        error_msg = None
+        if 400 <= response.status_code < 500:
+            error_msg = '%s Client Error: %s\n%s'
+        elif 500 <= response.status_code < 600:
+            error_msg = '%s Server Error: %s\n%s'
+
+        if error_msg:
+            raise requests.exceptions.HTTPError(
+                error_msg % (response.status_code, response.reason, response.text),
+                response=response
+            )
+
+        # Clover always returns JSON object in response
+
+        return json.loads(response.content, object_hook=CloverResponseObject)
+
+
+# # Example! Increases the price of all items in the inventory by 1 cent
+# if __name__ == '__main__':
+#     # TODO: remove during production
+#     CloverAPI.base_url = "https://www.clover.com"
+
+#     # Create CloverAPI instance with Merchant ID and Access Token
+#     c = CloverAPI("9a996285-f9d5-9b8c-f9ad-d6ef752dfb1a", "32VSMVNFJ4ERP")
+
+#     # Get all items from merchant's inventory
+#     response = c.get("/v2/merchant/{mId}/inventory/items")
+
+#     for item in response.items:
+#         c.post("/v2/merchant/{mId}/inventory/items/{itemId}/price",
+#                {"price": item.price + 1}, itemId=item.id)
---
+++ main.py
@@ -0,0 +1,215 @@
+# curl -s https://api.clover.com/v3/merchants/W96Q5MJDPAN4P/orders --header "Authorization:Bearer 86a5af7b-14e9-16e9-582d-92e4dc8a8b68" | python -mjson.tool
+
+from flask import Flask
+from flask import abort, request
+from flask import redirect
+from flask import render_template
+from flask import flash
+
+import os
+
+from urlparse import urlparse
+
+import clover_api
+# import json
+# import jsonify
+
+from flask import session
+
+import requests
+import requests.auth
+
+app = Flask(__name__)
+app.requests_session = requests.Session()
+
+app.secret_key = os.urandom(24)
+
+
+APP_ID = "PPY2CD6SA86FY"
+APP_SECRET = "4065d73d-ef74-937b-6ac5-d9982540348f"
+MERCHANT_ID = "W96Q5MJDPAN4P"
+REDIRECT_URI = "http://localhost:5000/clover_callback"
+BASE_URL = "https://www.clover.com"
+
+
+@app.route('/')
+def homepage():
+
+    # text = '<a href="%s">Authenticate with Clover</a>'
+    # return text % make_authorization_url()
+    auth_url = make_authorization_url()
+    # print auth_url
+
+    return render_template('homepage.html', 
+                            auth_url=auth_url)
+
+
+def make_authorization_url():
+    """Serve a webpage on localhost that creates an authorization link
+    out to clover.com"""
+
+    # Generate a random string for the state parameter
+    # Save it for use later to prevent xsrf attacks
+
+    from uuid import uuid4   
+    state = str(uuid4())
+    # save_created_state(state)
+    
+    params = {"client_id": APP_ID,
+              "response_type": "code",
+              "state": state,
+              "redirect_uri": REDIRECT_URI,
+              "duration": "temporary",
+              "scope": "identity"}
+
+    import urllib
+
+    url = "https://www.clover.com/oauth/authorize?client_id" + APP_ID + urllib.urlencode(params)
+    return url
+
+def save_created_state(state):
+    pass
+
+def is_valid_state(state):
+    return True
+
+def get_token(code):
+
+    parameters = {
+        'redirect_uri': REDIRECT_URI,
+        'code': code,
+        'grant_type': 'authorization_code',
+    }
+
+    response = requests.post(
+        "https://www.clover.com/oauth/token",
+        auth = {
+            APP_ID,
+            APP_SECRET
+        },
+        data = parameters
+    )
+
+    return response
+
+
+    # client_auth = requests.auth.HTTPBasicAuth(APP_ID, APP_SECRET)
+    # post_data = {"grant_type": "authorization_code",
+    #              "code": code,
+    #              "redirect_uri": REDIRECT_URI}
+    # response = requests.get("https://www.clover.com/oauth/token",
+    #                          auth=client_auth,
+    #                          data=post_data)
+    # print response
+    # return response
+
+# def get_redirect_uri(request):
+#     """Return OAuth redirect URI."""
+#     parsed_url = urlparse(request.url)
+#     if parsed_url.hostname == 'localhost':
+#         return 'http://{hostname}:{port}/submit'.format(
+#             hostname=parsed_url.hostname, port=parsed_url.port
+#         )
+#     return 'https://{hostname}/submit'.format(hostname=parsed_url.hostname)
+
+@app.route('/clover_callback')
+def clover_callback():
+   
+    error = request.args.get('error', '')
+    if error:
+        return "Error: " + error
+    state = request.args.get('state', '')
+    if not is_valid_state(state):
+        abort(403)
+
+    code = request.args.get('code')
+
+
+    # post_data = {
+    #     'redirect_uri': REDIRECT_URI,
+    #     'code': code,
+    #     'grant_type': 'authorization_code',
+    # }
+
+    params = {
+        'client_id': APP_ID,
+        'client_secret': APP_SECRET,
+        'code': code
+    }
+
+    response = requests.post(
+        "https://www.clover.com/oauth/token",
+        # auth = {
+        #     APP_ID,
+        #     APP_SECRET,
+        #     code
+        # },
+        # data = post_data
+        data = {
+            'client_id': APP_ID,
+            'client_secret': APP_SECRET,
+            'code': code
+        }
+    )
+
+    token_json = response.json()
+    access_token = token_json['access_token']
+
+    session['access_token'] = access_token
+
+    # return "got an access token! %s" % access_token
+    return redirect('/orders')
+
+
+@app.route('/show_token')
+def show_token():
+
+    return "Access token is %s" % session['access_token']
+
+
+
+@app.route('/orders')
+def orders():
+
+    c = clover_api.CloverAPI(session['access_token'], MERCHANT_ID)
+
+    response = c.get("/v3/merchants/{}/items".format(MERCHANT_ID))
+
+    orders = response['elements']
+
+    for order in orders:
+        print order['name'], order['price'], order['id']
+
+
+    # # return 'success'
+
+    # print session['access_token']
+    # access_token = session['access_token']
+
+    # headers = {"Authorization": "bearer " + access_token}
+
+    # response = requests.get("https://api.clover.com/v3/merchants/{}/orders".format(MERCHANT_ID), headers=headers)
+    # print "response", response
+    # # print json.dump(data)
+
+    # return 'success'
+
+    return render_template("all_orders.html", orders=orders)
+
+@app.route("/order/<int:order_id>")
+def show_order(order_id):
+
+#     return render_template("order_details.html",
+#                             order=order)
+
+    c = clover_api.CloverAPI(session['access_token'], MERCHANT_ID)
+
+    response = c.get("/v3/merchants/{}/items".format(MERCHANT_ID))
+
+    order = response['elements'][str(order_id)]
+
+    return "order %s" % order
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
